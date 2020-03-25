203f17de2e73214746aaf793170c752fc83adae2
joaquin-rodriguez <joaquin.rodriguez@aulmnos.uneatlantico.es>
2020-03-25 12:35:46 -0700

    working survey

--- router/src/Survey.js
+++ router/src/Survey.js
@@ -1,20 +1,28 @@
 import React from "react";
 
+function display() {
+  let name = document.getElementById("nombre").value;
+  let lastName = document.getElementById("apellido").value;
+  let age = document.getElementById("edad").value;
+  let gender = document.getElementById("genero").value;
+  alert("your info is " + name + " " + lastName + " " + age + " " + gender);
+}
+
 function Survey() {
   return (
     <div className="questions">
       <form>
         <p>Name</p>
-        <input type={Text} className="name" id="test" />
+        <input type={Text} className="name" id="nombre" />
         <p>Last Name</p>
-        <input type={Text} className="lastName" />
+        <input type={Text} className="lastName" id="apellido" />
         <p>Age</p>
-        <input type={Text} className="age" />
+        <input type={Text} className="age" id="edad" />
         <p>Gender</p>
-        <input type={Text} className="gender" />
+        <input type={Text} className="gender" id="genero" />
         <p>Blood Type</p>
         <br></br>
-        <button>Submit</button>
+        <button onClick={display}>Submit</button>
         <br></br>
       </form>
     </div>
