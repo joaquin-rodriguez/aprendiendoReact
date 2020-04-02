7a0b9191f2f4d2ca788d3df9d479028a066160b6
joaquin-rodriguez <joaquin.rodriguez@aulmnos.uneatlantico.es>
2020-04-02 12:37:02 -0700

    added new text elements to the welcome page

--- router/src/App.js
+++ router/src/App.js
@@ -4,6 +4,8 @@ import Nav from "./Nav";
 import Content from "./Content";
 import Survey from "./Survey";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { render } from "@testing-library/react";
+import bootstrap from "react-bootstrap";
 
 function App() {
   return (
@@ -15,6 +17,20 @@ function App() {
           <Route path="/content" component={Content} />
           <Route path="/survey" component={Survey} />
         </Switch>
+        <render>
+          <div className="news">
+            <h3>News</h3>
+            <h4>Stay Up To Date On The Latest New About Notrix!</h4>
+            <p>
+              Notrix has implemented a new and revolutionary survey were you can
+              enter your info for no reason whatsoever!{" "}
+            </p>
+          </div>
+          <img
+            className="medical"
+            src="https://image.freepik.com/free-vector/medical-stuff-elements-background_18591-37024.jpg"
+          ></img>
+        </render>
       </div>
     </Router>
   );
